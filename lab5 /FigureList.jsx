23544a8d9a3b3b77d359c2b64aa3e76e24b4f14d
Sinchana2250 <sinchana.23cs158@sode-edu.in>
2026-02-17 12:38:12 +0530

    Add FigureList component for dynamic image gallery

---
+++ lab5 /FigureList.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import BasicFigure from "./BasicFigure";
+import "./FigureList.css"; // Import CSS for styling
+
+const initialImages = [
+  {
+    src: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+    caption: "Image 1",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1523961131990-5ea7c61b2107",
+    caption: "Image 2",
+  },
+  {
+    src: "https://th.bing.com/th/id/OIP.nKc0BH_t6LAViz3N2SedfAHaE7?w=247&h=180&c=7&r=0&o=7&cb=defcache2&pid=1.7&rm=3&defcache=1",
+    caption: "Image 3",
+  },
+];
+
+const FigureList = () => {
+  const [images, setImages] = useState(initialImages);
+
+  const addImage = () => {
+    const randomId = Math.floor(Math.random() * 1000);
+    const newImage = {
+      src: `https://picsum.photos/400/300?random=${randomId}`,
+      caption: `Image ${images.length + 1}`,
+    };
+    setImages([...images, newImage]);
+  };
+
+  const removeImage = (index) => {
+    setImages(images.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="container">
+      <h1>Dynamic Image Gallery</h1>
+
+      <div className="buttons">
+        <button onClick={addImage}>Add Image</button>
+      </div>
+
+      <div className="gallery">
+        {images.map((image, index) => (
+          <BasicFigure
+            key={index}
+            src={image.src}
+            caption={image.caption}
+            onRemove={() => removeImage(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FigureList;
